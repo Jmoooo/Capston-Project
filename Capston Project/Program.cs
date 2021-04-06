c5894d5026449deb4f25865dcca56b96df79808d
Jmoooo <jcmoranville@gmail.com>
2021-04-05 21:48:58 -0400

    Fixed now backing up

--- Capston Project/Program.cs
+++ Capston Project/Program.cs
@@ -22,17 +22,46 @@ namespace Capston_Project
         {
             //Set to get username and passwords 
             //might set up for now just store that data
-            
-        }
-        /*public static ReadFiles(string)
-        {
 
         }
-        public static void encryption()
-        {
+        /* static void ViewFile()
+         {
+             var fileToOpen = "SomeFilePathHere";
+             var process = new Process("notepad.exe");
+             process.StartInfo = new ProcessStartInfo("notepad.exe")
+             {
+                 UseShellExecute = true,
+                 FileName = fileToOpen
+             };
 
-        }
-        */
+             process.Start();
+             process.WaitForExit();
+         }
+
+         static void ReadFiles()
+         {
+             int counter = 0;
+             string line;
+
+             // Read the file and display it line by line.  
+             System.IO.StreamReader file =/*file from upload or download*/
+        // new System.IO.StreamReader(/*file from upload or download);
+        /* while ((line = file.ReadLine()) != null)
+         {
+             System.Console.WriteLine(line);
+             counter++;
+         }
+
+         file.Close();
+         System.Console.WriteLine("There were {0} lines.", counter);
+         // Suspend the screen.  
+         System.Console.ReadLine();
+     }*/
+        /* public static void encryption()
+         {
+
+         }
+         */
 
     }
 }
